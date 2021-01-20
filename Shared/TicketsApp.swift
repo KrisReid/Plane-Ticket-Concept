fefc8a67b8dfc4ffcb964c919aa0d1728137c904
Kris Reid <krisreid_06@hotmail.com>
2021-01-20 09:27:01 +0000

    Initial Commit

---
+++ Shared/TicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketsApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
